97b19df1ceaf436d1c072ba5ecd4f5199f835a19
Doc Walker <doc.walker@jameshardie.com>
2014-01-05 16:00:40 -0600

    Fix #7 add support for chefspec

--- spec/hosts_spec.rb
+++ spec/hosts_spec.rb
@@ -1,25 +1,24 @@
-require 'chefspec'
-require 'fauxhai'
+require 'spec_helper'
 
 describe 'network::hosts' do
-  before do
-    Fauxhai.mock do |node|
-      node['file'] = {
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      # override cookbook attributes
+      node.set['file'] = {
         'header' => 'node.file.header'
       }
-    end # Fauxhai.mock
-  end # before
-  
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'network::hosts' }
-  
+    end.converge(described_recipe)
+  end # let
+
   it 'should include recipe network::common' do
     chef_run.should include_recipe 'network::common'
   end # it 'should include recipe network::common'
-  
+
   it 'should create /etc/hosts owned by root:wheel' do
     file = '/etc/hosts'
     chef_run.should create_file_with_content file, 'node.file.header'
-    chef_run.template(file).should be_owned_by 'root', 'wheel'
+    expect(chef_run.template(file).owner).to eq('root')
+    expect(chef_run.template(file).group).to eq('root')
   end # it 'should create /etc/hosts owned by root:wheel'
-  
+
 end # describe 'network::hosts'
